82623d7a1024eff008bd91c695583c378981f4d3
Joe Williams <williams.joe@gmail.com>
2015-12-03 23:04:11 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+### RaspberryPi NTP+GPS setup
+
+#### Hardware
+* RaspberryPi 2 B
+* GPS board
+  * http://ava.upuaut.net/store/index.php?route=product/product&path=59_60&product_id=117
+* Antenna
+  * http://www.amazon.com/gp/product/B00JE4GV8S
+
+#### Usage
+* Clone this repo to your rpi
+* `sudo ./bootstrap.sh`
+
+#### Tools
+* `sudo ppstest /dev/pps0`
+* `sudo minicom /dev/ttyAMA0`
+* `cgps -s # when ntpd isn't using the serial iface`
+* `/opt/ntp/ntp-4.2.8p4/bin/ntpq -p`
+
+#### Resources
+* http://www.satsignal.eu/ntp/Raspberry-Pi-quickstart.html
+* http://ava.upuaut.net/?p=726
+* http://www.satsignal.eu/ntp/Raspberry-Pi-NTP.html
+* https://www.eecis.udel.edu/~mills/ntp/html/monopt.html
+* http://www.catb.org/gpsd/gpsd-time-service-howto.html
---
+++ bootstrap.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+NTP_VER='4.2.8p4'
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+rpi-update
+
+cat packages | while read package; do
+  apt-get -y install $package
+done
+
+apt-get remove --purge ntp
+
+echo 'dtoverlay=pps-gpio,gpiopin=18' >> /boot/config.txt
+echo 'pps-gpio' >> /etc/modules
+
+LINENO=`grep -n ttyAMA0 /etc/inittab | cut -d: -f1`
+sed "${LINENO}d" /etc/inittab | sudo tee /etc/inittab
+
+cmdlinefile1=`sed s/console\=ttyAMA0\,115200// /boot/cmdline.txt`
+echo ${cmdlinefile1} | sed s/kgdboc\=ttyAMA0\,115200// | sudo tee /boot/cmdline.txt
+
+ln -s /dev/ttyAMA0 /dev/gps0
+
+wget http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-${NTP_VER}.tar.gz
+tar zxf ntp-${NTP_VER}.tar.gz
+cd ntp-${NTP_VER}
+
+mkdir -p /opt/ntp/etc /opt/ntp/ntp-${NTP_VER}
+
+./configure --prefix=/opt/ntp/ntp-${NTP_VER} \
+  --enable-linuxcaps \
+  --disable-all-clocks \
+  --disable-parse-clocks \
+  --enable-NMEA \
+  --enable-ATOM \
+  --enable-GPSD \
+  --enable-ntpdate-step \
+  --disable-hourly-todr-sync \
+  --disable-nls \
+  --disable-autokey \
+  --without-net-snmp-config \
+  --with-crypto
+
+make -j 5
+make install
+
+mkdir /opt/ntp/lib
+chown nobody /opt/ntp/lib
+
+mkdir /var/log/ntpstats
+chown nobody /var/log/ntpstats
+
+cp -a etc /opt/ntp/
+cp -a service /opt/ntp/
+ln -s /opt/ntp/service /etc/service/ntpd
+
+echo 'DEVICES="/dev/ttyAMA0"' >> /etc/default/gpsd
+echo 'START_DAEMON="true"' >> /etc/default/gpsd
+
+echo ''
+echo 'done! please reboot'
